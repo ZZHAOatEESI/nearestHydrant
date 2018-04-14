0486b96db4657c5b25cac05d43e5f6db337ed993
Zhengqiao Zhao <Joe@n3-32-42.dhcp.drexel.edu>
2018-04-14 15:41:46 -0400

    Add NearestElement Backend Class and Functions

---
+++ demo.py
@@ -0,0 +1,5 @@
+from NearestElement import *
+hydrants = data_generator('/Users/Joe/Desktop/phillyCODEFEST/hydrants.json')
+hydrants.get_nearest_fast_allinOne(-75.1, 39.9, epsilon = 0.5, top = 3, metric = 'km')
+# bounding box debugging
+# lng_l, lat_u, lat_l, _, _, _ = hydrants.reduce_search_space(0.5, -75.109585, 39.951305, metric = 'km')
